e15a52f05444abb6d6d1c9e06c19311b2c2eab98
apacheco-2022046 <apacheco-2022046@kinal.edu.gt>
2024-06-09 22:21:47 -0600

    Responde al seleccionar la imagen

---
+++ VIDA-ReactNative/services/api.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+// Suponiendo que apiUrl ya está configurado con la URL base de tu API
+const apiUrl = axios.create({
+    baseURL: 'http://localhost:2880',
+    headers: {
+        'Content-Type': 'multipart/form-data'
+    }
+});
+
+/*export const saveAlertaRequest = async (alerta) => {
+    try {
+        const formData = new FormData();
+        
+        // Añadir los otros campos de alerta al FormData
+        Object.keys(alerta).forEach(key => {
+            if (key !== 'fotoDesaparecido') {
+                formData.append(key, alerta[key]);
+            }
+        });
+
+        // Añadir la imagen al FormData
+        if (alerta.fotoDesaparecido) {
+            formData.append('file', {
+                uri: alerta.fotoDesaparecido.uri,
+                name: alerta.fotoDesaparecido.name,
+                type: alerta.fotoDesaparecido.type || 'image/jpg' // Ajusta el tipo MIME según sea necesario
+            });
+        }
+
+        const response = await apiUrl.post('/save', formData);
+        return response.data;
+    } catch (error) {
+        return {
+            error: true,
+            message: error.message,
+            response: error.response
+        };
+    }
+};*/
+
+
+
+export const saveAlertaRequest = async (alerta) => {
+    try {
+        const response = await apiUrl.post('/save', alerta);
+        return response.data;
+    } catch (error) {
+        return {
+            error: true,
+            message: error.message,
+            response: error.response
+        };
+    }
+} 
